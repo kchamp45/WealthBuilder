9fa19168685d2edf8eee5501c5df3cb348b53c60
Kimberly Lu <kimlu4@comcast.net>
2017-11-05 10:43:30 -0800

    create expense layout for user to input typical expenses.

--- app/src/main/java/com/epicodus/wealthbuilder/ui/ExpenseActivity.java
+++ app/src/main/java/com/epicodus/wealthbuilder/ui/ExpenseActivity.java
@@ -1,15 +1,23 @@
 package com.epicodus.wealthbuilder.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.epicodus.wealthbuilder.R;
 
 public class ExpenseActivity extends AppCompatActivity {
+    private TextView mIncomeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense);
+        mIncomeTextView = (TextView) findViewById(R.id.incomeTextView);
+        Intent intent = getIntent();
+        String income = intent.getStringExtra("income");
+        mIncomeTextView.setText("Here is your income: " + income);
+
     }
 }
--- app/src/main/java/com/epicodus/wealthbuilder/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/wealthbuilder/ui/MainActivity.java
@@ -3,13 +3,17 @@ package com.epicodus.wealthbuilder.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.epicodus.wealthbuilder.R;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private EditText mIncomeEditText;
     private Button mSubmitIncomeButton;
 
     @Override
@@ -17,12 +21,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mIncomeEditText = (EditText) findViewById(R.id.incomeEditText);
         mSubmitIncomeButton = (Button) findViewById(R.id.submitIncomeButton);
         mSubmitIncomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            Toast.makeText(MainActivity.this, "Submitted!", Toast.LENGTH_LONG).show();
+                String income = mIncomeEditText.getText().toString();
+                Log.d(TAG, income);
+            Toast.makeText(MainActivity.this, "$$Hooray$$!", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(MainActivity.this, ExpenseActivity.class);
+                intent.putExtra("income", income);
                 startActivity(intent);
             }
         });
