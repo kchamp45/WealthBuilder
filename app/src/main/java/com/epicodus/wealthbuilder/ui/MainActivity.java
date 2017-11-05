e159a9b33cc86203d67f16bef50aab6f2415d3df
Kimberly Lu <kimlu4@comcast.net>
2017-11-05 09:48:57 -0800

    create main activity layout to accept user income input and intent to move to expense activity.

--- app/src/main/java/com/epicodus/wealthbuilder/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/wealthbuilder/ui/MainActivity.java
@@ -1,15 +1,30 @@
 package com.epicodus.wealthbuilder.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.epicodus.wealthbuilder.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mSubmitIncomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSubmitIncomeButton = (Button) findViewById(R.id.submitIncomeButton);
+        mSubmitIncomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Submitted!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, ExpenseActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
